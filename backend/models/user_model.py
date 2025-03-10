edb8f98c0c89b6333abe8e79076e6d1bff3d3f80
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-03-10 16:36:12 +0000

    added workoutsession and exercise ORM models

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import List, Optional
+from backend.models.workout_model import WorkoutPlan
 
 class UserBase(SQLModel):
     username: str = Field(index=True, unique=True)
@@ -8,7 +9,7 @@ class UserSQL(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     hashed_password: str
 
-    workout_plans: List["WorkoutPlan"] = Relationship(back_populates="user", cascade_delete="all, delete")
+    workout_plans: List["WorkoutPlan"] = Relationship(back_populates="user", cascade="all, delete")
 
 class UserInput(UserBase):
     plain_password: str
--- backend/models/workout_model.py
+++ backend/models/workout_model.py
@@ -11,3 +11,26 @@ class WorkoutPlan(SQLModel, table=True):
     equipment_requirements: str
 
     user: UserSQL = Relationship(back_populates="workout_plans")
+    workout_sessions = List["WorkoutSession"] = Relationship(back_populates="workout_plan", cascade="all, delete")
+
+class WorkoutSession(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    workout_id: int = Field(foreign_key="workout.id", sa_column=ForeignKey("workout.id", ondelete="CASCADE"))
+    session_name: str
+    length_of_session: float
+    day_of_week: str
+    equipment_requirements: str
+
+    workout_plan: WorkoutPlan = Relationship(back_populates="workout_sessions")
+    exercises: List["Exercise"] = Relationship(back_populates="workout_session", cascade="all, delete")
+
+class Exercise(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    session_id: int = Field(foreign_key="workoutsession.id", sa_column=ForeignKey("workoutsession.id", ondelete="CASCADE"))
+    exercise_name: str
+    sets: int
+    reps: int
+    reps_in_reserve: Optional[int] = Field(default=0)
+
+    workout_session: WorkoutSession = Relationship(back_populates="exercises")
+
