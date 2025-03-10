7fea79c9a2ddf609687620516e11aa76cd25dc1b
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-03-10 18:39:59 +0000

    added table names to ORM models

--- backend/models/db_models.py
+++ backend/models/db_models.py
@@ -5,19 +5,24 @@ from typing import List, Optional
 class UserBase(SQLModel):
     username: str = Field(index=True, unique=True)
 
+class UserInput(UserBase):
+    plain_password: str
+
 class UserSQL(UserBase, table=True):
+
+    __tablename__ = "user"
+
     id: Optional[int] = Field(default=None, primary_key=True)
     hashed_password: str
 
     workout_plans: List["WorkoutPlan"] = Relationship(back_populates="user")
 
-class UserInput(UserBase):
-    plain_password: str
-
-
 class WorkoutPlan(SQLModel, table=True):
+
+    __tablename__ = "workoutplan"
+
     id: Optional[int] = Field(default=None, primary_key=True)
-    user_id: int = Field(foreign_key="usersql.id")
+    user_id: int = Field(foreign_key="user.id")
     no_of_sessions: int
     average_session_length: float
     equipment_requirements: str
@@ -27,6 +32,9 @@ class WorkoutPlan(SQLModel, table=True):
     
 
 class WorkoutSession(SQLModel, table=True):
+
+    __tablename__ = "workoutsession"
+
     id: Optional[int] = Field(default=None, primary_key=True)
     workout_id: int = Field(foreign_key="workoutplan.id")
     session_name: str
@@ -38,6 +46,9 @@ class WorkoutSession(SQLModel, table=True):
     exercises: List["Exercise"] = Relationship(back_populates="workout_session")
 
 class Exercise(SQLModel, table=True):
+
+    __tablename__ = "exercise"
+
     id: Optional[int] = Field(default=None, primary_key=True)
     session_id: int = Field(foreign_key="workoutsession.id")
     exercise_name: str
