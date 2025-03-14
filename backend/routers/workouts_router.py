bfbf7bc8ba048c7a3b06fe3f73524e64af2ad6b1
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-03-14 10:48:06 +0000

    Added basic workoutsession info api endpoint and method (needs a lot of work still obviously, just copied code from exercise info)

--- backend/routers/workouts_router.py
+++ backend/routers/workouts_router.py
@@ -6,7 +6,7 @@ from pymongo import database
 from database.mongodb.workout_context_db import add_workout_context, get_workout_context
 from models.input_models import WorkoutGenInput
 from ai.gen_workout_plan import generate_workout_plan
-from ai.gen_workout_info import generate_exercise_overview
+from ai.gen_workout_info import generate_exercise_overview, generate_workoutsession_overview
 
 router = APIRouter(
     prefix="/workouts"
@@ -30,6 +30,15 @@ def create_workout(workout_input: WorkoutGenInput, user_id: int, db: Session = D
         f"Workout context {add_result_mongodb["status"]}, Id: {add_result_mongodb["inserted_id"]}",
         status_code=200
     )
+
+@router.get("/get-workoutsession-info")
+def get_workoutsession_info(workout_plan_id: int , workoutsession_id: int) :
+
+    context = get_workout_context(workout_plan_id)
+
+    ai_response_data = generate_workoutsession_overview(context, workoutsession_id)
+
+    return Response(ai_response_data, status_code=200)
 
 @router.get("/get-exercise-info")
 def get_exercise_info(workout_plan_id: int , exercise_id: int) :
