f22a8aad992eb2bb6460bdd6104f9cddf1e31155
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-03-23 21:04:22 +0000

    Experimented with text splitting and models to determine the best way to retrieve sources and reference them correctly.

--- backend/ai_services/agents/train_availability_agent.py
+++ backend/ai_services/agents/train_availability_agent.py
@@ -22,7 +22,7 @@ if not os.path.exists(persistent_directory):
     loader = TextLoader("gymshark.txt", encoding="utf-8")
     document = loader.load()
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
     docs = text_splitter.split_documents(document)
 
     vectorstore = Chroma.from_documents(docs, embeddings, persist_directory="./chroma_db")
@@ -31,29 +31,37 @@ else:
     vectorstore = Chroma(persist_directory=persistent_directory, embedding_function=embeddings)
 
 retriever = vectorstore.as_retriever(search_type= "similarity_score_threshold",
-                                     search_kwargs={"k": 3, "score_threshold": 0.3})
+                                     search_kwargs={"k": 1, "score_threshold": 0.2})
 
-query = "What are the negatives of a Body Part split workout plan?"
+vector_query = "Body part split disadavantages"
 
-relevant_docs = retriever.invoke(query)
+ai_query = "What are the disadvanatages of a body part split?"
+
+relevant_docs = retriever.invoke(vector_query)
 
 print("\n--- Relevant Documents ---")
 for i, doc in enumerate(relevant_docs, 1):
     print(f"Document {i}:\n{doc.page_content}\n")
 
-prompt = (
-    "Here are some pieces of content that may help you answer the following question:"
-    + query 
-    +"\n\n Relevant Document: \n"
+context = (
+    "Here are some relevant documents that may help you answer the question:"
+    +"\n\n Relevant Documents: \n"
     + "\n\n".join([doc.page_content for doc in relevant_docs])
-    + "\n\n Only provide an answer based on the following documents. If your answer is not from the document, please state this in your answer."
+    + "\n\n Some documents contain references to other sources by numbers e.g. [1], [2] etc. These numbers correspond with references that are marked with the same numbers at the end of documents e.g. [2] Example university (year), Paper title etc.."
+    + "\n\n Your job is to provide an answer based on the following documents. If your answer is not from the document, state this in your answer."
+    + "\n\n When generating your answer, include inline number citations to the references from the relevant documents you used to come up with your answer (e.g. [1], [2] etc.)"
+    + "\n\n Provide the references used in your response at the end with the same number they have in the inline citations you generated."
+    + "\n\n Number these references to match the inline references you gave in the response. e.g.:"
+    + "\n\n Response: This is text response content gathered from a reference [1]"
+    +"\n\n References: [1] Reference author (reference year) paper title etc."
 )
 
-model = ChatOpenAI(model="gpt-3.5-turbo")
+model = ChatOpenAI(model="gpt-4o-mini")
 
 messages = [
-    HumanMessage(content="You are a helpful muscle hypertrophy knowledge assistant who provides answers to questions."),
-    SystemMessage(content=prompt)
+    SystemMessage(content="You are a helpful assistant who provides answers to workout split questions and provides the references for the knowledge used in your answers and at the end of the answer.\n"),
+    SystemMessage(content=context),
+    HumanMessage(content=f"\n\n This is your question to answer: {ai_query}")
 ]
 
 llm_response = model.invoke(messages)
