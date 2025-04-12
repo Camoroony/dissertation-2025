67e15eadcae13f05b3d2484a2c38820f7de409ef
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-04-12 19:09:39 +0100

    added ratings table to sql db schema

--- backend/models/db_models.py
+++ backend/models/db_models.py
@@ -14,6 +14,7 @@ class UserSQL(UserBase, table=True):
     hashed_password: str
 
     workout_plans: List["WorkoutPlan"] = Relationship(back_populates="user", sa_relationship_kwargs={"cascade": "delete"})
+    ratings: List["Rating"] = Relationship(back_populates="user", sa_relationship_kwargs={"cascade": "delete"})
 
 # Workout SQL Models 
 
@@ -22,6 +23,7 @@ class WorkoutPlan(SQLModel, table=True):
     __tablename__ = "workoutplan"
 
     id: Optional[int] = Field(default=None, primary_key=True)
+    plan_name: str 
     user_id: int = Field(foreign_key="user.id")
     no_of_sessions: int
     average_session_length: int
@@ -29,6 +31,7 @@ class WorkoutPlan(SQLModel, table=True):
 
     user: UserSQL = Relationship(back_populates="workout_plans")
     workout_sessions: List["WorkoutSession"] = Relationship(back_populates="workout_plan", sa_relationship_kwargs={"cascade": "delete"})
+    ratings: List["Rating"] = Relationship(back_populates="workout_plan", sa_relationship_kwargs={"cascade": "delete"})
     
 
 class WorkoutSession(SQLModel, table=True):
@@ -57,3 +60,18 @@ class Exercise(SQLModel, table=True):
     reps_in_reserve: Optional[int] = Field(default=0)
 
     workout_session: WorkoutSession = Relationship(back_populates="exercises", sa_relationship_kwargs={"cascade": "delete"})
+
+# Workout ratings table
+
+class Rating(SQLModel, table=True):
+
+    __tablename__ = "rating"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    workoutplan_id: int = Field(foreign_key="workoutplan.id")
+    user_id: int = Field(foreign_key="user.id")
+    rating: bool
+    comment: str
+
+    workout_plan: WorkoutPlan = Relationship(back_populates="ratings", sa_relationship_kwargs={"cascade": "delete"})
+    user: UserSQL = Relationship(back_populates="ratings", sa_relationship_kwargs={"cascade": "delete"})
