4d30ca13b0b53c29127510a132c6f3774448c98e
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-04-13 18:26:17 +0100

    Added missing ratings router to main.py

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Response
 from routers.users_router import router as users_router
 from routers.workouts_router import router as workouts_router
 from routers.chatbot_router import router as chatbot_router
+from routers.ratings_router import router as ratings_router
 from database.sql.init_sql_db import create_db
 from database.mongodb.references_db import references_init
 
@@ -10,6 +11,7 @@ app = FastAPI()
 app.include_router(users_router)
 app.include_router(workouts_router)
 app.include_router(chatbot_router)
+app.include_router(ratings_router)
 
 create_db()
 references_init()
