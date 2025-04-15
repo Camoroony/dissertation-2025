e9f79daeda31b23ca05fdb9699a2f279c0bdbae8
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-04-15 15:31:28 +0100

    More updates to create account page

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import "../css/Navbar.css"
 function NavBar () {
     return <nav className="navbar">
         <div className="navbar-logo">
-            <Link to ="/" className="font-medium text-[#2A955F] hover:text-[#1FA562] no-underline transition-colors duration-200">
+            <Link to ="/">
             Hypertrophy <b>Education</b>
             </Link>
         </div>
--- frontend/src/pages/CreateAccountPage.jsx
+++ frontend/src/pages/CreateAccountPage.jsx
@@ -1,18 +1,27 @@
 function CreateAccountPage() {
     return (
-    <div className="flex justify-center items-center h-screen">
+    <>
+    <div className="flex flex-col justify-center items-center mt-20">
+    <h1 className="text-6xl font-bold mb-15">Create a <span className="text-[#2A955F]">new account!</span></h1>
+    <p className="text-lg">Enter your details below to create an account!</p>
+    </div>
+
+
+    <div className="flex justify-center items-center mt-25">
         <div className="shadow-lg px-8 py-5 border w-96">
             <h2 className="text-lg font-bold mb-4">Create an account</h2>
             <form>
-                <div className="mb-4">
+                <div className="mb-5">
                     <label htmlFor="username" className="block text-gray-700">Username</label>
-                    <input type="text" placeholder="Enter Username" className="w-full px-3 py-2 border"/>
+                    <input type="text" placeholder="Enter Username" className="w-full px-3 py-2 border rounded"/>
                 </div>
-                <div>
+                <div className="mb-6">
                     <label htmlFor="password" className="block text-gray-700">Password</label>
-                    <input type="text" placeholder="Enter Password" className="w-full px-3 py-2 border"/>
+                    <input type="text" placeholder="Enter Password" className="w-full px-3 py-2 border rounded"/>
                 </div>
-                <button className="w-full bg-green-600 text-white py-2">Create Account</button>
+                <div className="mb-5">
+                <button className="w-full bg-black text-white py-2 rounded">Create Account</button>
+                </div>
             </form>
             <div className="text-center">
                 <p>Already have an account?</p>
@@ -20,6 +29,7 @@ function CreateAccountPage() {
             </div>
         </div>
     </div>
+    </>
     )
 }
 
