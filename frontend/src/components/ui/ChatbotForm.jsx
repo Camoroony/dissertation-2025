98055987ebf30bb01b6c7133e88aeb17181cef2b
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-04-21 15:50:43 +0100

    Added functionality to send message and log it in console (will be changed to use API call soon)

--- frontend/src/components/ui/ChatbotForm.jsx
+++ frontend/src/components/ui/ChatbotForm.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useRef } from 'react'
 import '../../css/Chatbot.css'
 
 const ChatbotForm = () => {
+
+    const inputRef = useRef();
+
+    const handleChatbotSubmit = (e) => {
+        e.preventDefault();
+        const userMessage = inputRef.current.value.trim();
+        if (!userMessage) return;
+        inputRef.current.value = "";
+
+        console.log(userMessage);
+    }
+
+
     return (
-        <form action="" className="chat-form">
-            <input type="text" placeholder='Message...' className="message-input" required />
+        <form action="" className="chat-form" onSubmit={handleChatbotSubmit}>
+            <input ref={inputRef} type="text" placeholder='Message...' className="message-input" required />
             <button className='pi pi-angle-up' style={{ fontSize: '1rem' }}></button>
         </form>
     )
