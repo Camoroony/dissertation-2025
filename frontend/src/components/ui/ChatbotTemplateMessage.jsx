3b96d7e7c4e2f0657404ecdc732f21515d32657f
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-04-24 16:46:35 +0100

    Changed template messages to be more explanatory and understandable

--- frontend/src/components/ui/ChatbotTemplateMessage.jsx
+++ frontend/src/components/ui/ChatbotTemplateMessage.jsx
@@ -11,9 +11,11 @@ const ChatbotTemplateMessage = ({ chat_type }) => {
 
     return (
         <p className="message-text">
-            {chat_type == "General" ? "Hi!, i'm the general chatbot!"
-            : chat_type == "Community" ? "Hi! I'm the community chatbot!"
-            : "Hi!, I'm the workout plan chatbot!"}
+            {chat_type === "General" ?
+                "Hi! I'm the General Chatbot. I'm here to answer your questions about weightlifting and hypertrophy training!" :
+                chat_type === "Community" ?
+                    "Hi! I'm the Community Chatbot. I can help you with anything related to the community page like posts or recommendations!" :
+                    "Hi! I'm the Workout Plan Chatbot. I'm here to answer any questions you have about your personalised workout plan."}
         </p>
     )
 }
