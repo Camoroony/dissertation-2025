13cdffa2e4f58e4b80123359864360fc2c9886dc
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-04-26 11:39:25 +0100

    Added switch to show difeerent icons depending on the review

--- frontend/src/components/ui/CommunityRating.jsx
+++ frontend/src/components/ui/CommunityRating.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const CommunityRating = ({ index, rating }) => {
   return (
     <li className="w-full break-words flex items-start gap-2" key={index}>
-      <i className='pi pi-check text-green-600 pt-0.75'></i>
-      <i className='pi pi-times text-red-600 pt-1'></i>
+      {rating.rating == true ? (  <i className='pi pi-check text-green-600 pt-0.75'></i>) : (<i className='pi pi-times text-red-600 pt-1'></i>)}
       <span className='p-1'>{rating.comment} <b className='italic'>({rating.user.username})</b></span>
     </li>)
 
