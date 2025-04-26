14fa5299ccec308479bf2605e507093d9ae72a22
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-04-26 11:41:47 +0100

    Added margin for the comments to space them out better

--- frontend/src/components/ui/CommunityRating.jsx
+++ frontend/src/components/ui/CommunityRating.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CommunityRating = ({ index, rating }) => {
   return (
-    <li className="w-full break-words flex items-start gap-2" key={index}>
+    <li className="w-full break-words flex items-start gap-2 mb-4" key={index}>
       {rating.rating == true ? (  <i className='pi pi-check text-green-600 pt-0.75'></i>) : (<i className='pi pi-times text-red-600 pt-1'></i>)}
       <span className='p-1'>{rating.comment} <b className='italic'>({rating.user.username})</b></span>
     </li>)
