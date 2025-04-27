d9a6249149c2e747c98823e6dbb4a69069c07c74
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-04-27 19:07:38 +0100

    Edited manage account form inputs to have username and password values

--- frontend/src/pages/ManageAccountPage.jsx
+++ frontend/src/pages/ManageAccountPage.jsx
@@ -1,20 +1,20 @@
 import { Link, useNavigate } from 'react-router-dom'
-import {useState} from 'react'
+import { useState } from 'react'
 import { createAccount } from '../services/accountapi'
 
 function ManageAccountPage() {
-    
+
 
     const [values, setValues] = useState({
-            username: '',
-            plain_password: ''
+        username: '',
+        plain_password: ''
     })
 
     const [errorMessage, setErrorMessage] = useState('')
     const navigate = useNavigate()
 
     const handleChanges = (e) => {
-            setValues({...values, [e.target.name]:e.target.value})
+        setValues({ ...values, [e.target.name]: e.target.value })
     }
 
     const handleSubmit = async (e) => {
@@ -22,12 +22,12 @@ function ManageAccountPage() {
         try {
             const response = await createAccount(values);
             console.log(response)
-            if(response.status === 201){
+            if (response.status === 201) {
                 navigate('/login', {
                     state: { successMessage: 'Account created successfully! Please log in.' }
                 });
             }
-        } catch(err) {
+        } catch (err) {
             console.log(err)
             if (err.message) {
                 setErrorMessage(`Error occured when creating account:\n ${err.message}` || 'An error occurred, please try again.')
@@ -39,49 +39,63 @@ function ManageAccountPage() {
         }
 
     }
-    
+
     return (
-    <>
+        <>
 
-     <div className="relative">
-            {errorMessage && (
-                <div className="absolute top-4 left-1/2 transform -translate-x-1/2 bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded shadow-lg transition-opacity duration-300">
-                {errorMessage}
+            <div className="relative">
+                {errorMessage && (
+                    <div className="absolute top-4 left-1/2 transform -translate-x-1/2 bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded shadow-lg transition-opacity duration-300">
+                        {errorMessage}
+                    </div>
+                )}
             </div>
-            )}
-     </div>
 
-    <div className="flex flex-col justify-center items-center mt-20">
-    <h1 className="text-6xl font-bold mb-15">Manage your <span className="text-[#2A955F]">account!</span></h1>
-    <p className="text-lg">Want to change any of your account details?</p>
-    <p className="text-lg">You can update your details below!</p>
-    </div>
-
-    <div className="flex justify-center items-center mt-25">
-        <div className="shadow-lg px-8 py-5 border w-96">
-            <h2 className="text-lg font-bold mb-4">Account details</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-5">
-                    <label htmlFor="username" className="block text-gray-700">Username</label>
-                    <input type="text" placeholder="Enter Username" className="w-full px-3 py-2 border rounded" name='username'
-                    onChange={handleChanges}/>
-                </div>
-                <div className="mb-6">
-                    <label htmlFor="password" className="block text-gray-700">Password</label>
-                    <input type="password" placeholder="Enter Password" className="w-full px-3 py-2 border rounded" name='plain_password'
-                    onChange={handleChanges}/>
-                </div>
-                <div className="mb-5">
-                <button className="w-full bg-black text-white py-2 rounded cursor-pointer">Create Account</button>
-                </div>
-            </form>
-            <div className="text-center">
-                <p>Already have an account?</p>
-                <Link to='/login' className='text-[#1FA562] hover:underline'>Login</Link>
+            <div className="flex flex-col justify-center items-center mt-20">
+                <h1 className="text-6xl font-bold mb-15">
+                    Update Your <span className="text-[#2A955F]">Account</span></h1>
+                <p className="text-lg">Need to make changes to your account information?</p>
+                <p className="text-lg">Easily update your details below.</p>
             </div>
-        </div>
-    </div>
-    </>
+
+            <div className="flex justify-center items-center mt-25">
+                <div className="shadow-lg px-8 py-5 border w-120">
+                    <h2 className="text-lg font-bold mb-4">Account details</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-2">
+                            <label htmlFor="new_username" className="block text-gray-700">New Username</label>
+                            <input type="text" placeholder="Enter your new username..." className="w-full px-3 py-2 border rounded" name='new_username'
+                                onChange={handleChanges} />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="confirm_username" className="block text-gray-700">Confirm Username</label>
+                            <input type="text" placeholder="Confirm your new username..." className="w-full px-3 py-2 border rounded" name='confirm_username'
+                                onChange={handleChanges} />
+                        </div>
+
+                        <div className="mb-2">
+                            <label htmlFor="new_password" className="block text-gray-700">New Password</label>
+                            <input type="text" placeholder="Enter new password" className="w-full px-3 py-2 border rounded" name='new_password'
+                                onChange={handleChanges} />
+                        </div>
+                        <div className="mb-7">
+                            <label htmlFor="confirm_password" className="block text-gray-700">Confirm Password</label>
+                            <input type="password" placeholder="Confirm your new password..." className="w-full px-3 py-2 border rounded" name='confirm_password'
+                                onChange={handleChanges} />
+                        </div>
+
+                        <div className='flex flex-row w-full'>
+                            <div className="mb-5">
+                                <button className="bg-black text-white p-2 rounded cursor-pointer">Update Details</button>
+                            </div>
+                            <div className="mb-5 ml-auto">
+                                <button className="bg-red-700 hover:bg-red-600 text-white p-2 rounded cursor-pointer">Delete Account</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
 
